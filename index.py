a7a0c08a74256415fd81fe7bc27e35e35c2f48b9
Devansh Thapa <devanshthapa0307@gmail.com>
2020-11-06 12:01:24 +0530

    important files
    Added all files to the project

--- index.py
+++ index.py
@@ -20,12 +20,12 @@ def transaction(alertmessage=""):
 def transaction_details():
 	sender=request.form.get('sender')
 	receiver=request.form.get('receiver')
-	s=str(sender)
-	r=str(receiver)
+	sender=str(sender)
+	receiver=str(receiver)
 	amount=request.form.get('amount')
 	amount=int(amount)
-	if s==r:
-		return render_template("transactiondetails.html",option=2) 
+	if sender==receiver:
+		return render_template("transactiondetails.html",option=2,sender=sender) 
 	else:
 		conn=dbConn()
 		balance_sender=getdata(sender,conn)
@@ -51,18 +51,31 @@ def transaction_details():
 def transactionHistory():
 	conn=dbConn()
 	data=conn.execute("SELECT Sender,Receiver,Amount from transactions")
-	transactionlist=generateList(data)
+	transactionlist=generateTransactionList(data)
 	conn.close()
 	return render_template('transactionHistory.html',data=transactionlist)	
 
-@app.route('/userdata')
-def userData():
+@app.route('/userlist')
+def userList():
 	conn=dbConn()
 	data=conn.execute("SELECT Name,Email,Balance from userdata")
-	userlist=generateList(data)
+	userlist=generateUserList(data)
 	conn.close()	
-	return render_template('userdata.html',userlist=userlist)
+	return render_template('userlist.html',userlist=userlist)
 
+@app.route('/userdetails',methods=['GET','POST'])
+def userDetails():
+	user=request.form.get('user')
+	user=str(user)
+	conn=dbConn()
+	userdata=conn.execute('SELECT Name,Email,Balance from userdata WHERE Name="'+user+'"')
+	userdatalist=generateList(userdata)
+	transactiondata=conn.execute('SELECT Sender,Receiver,Amount from transactions WHERE Sender="'+user+'"OR Receiver="'+user+'"')
+	transactionlist=generateTransactionList(transactiondata)
+	optionlist=generateOption(conn.execute('SELECT Name from userdata WHERE NOT Name="'+user+'"'))
+	conn.close()
+	senderdata=flask.Markup('<input type="hidden" id="sender" name="sender" value="'+user+'">')
+	return render_template('userdetails.html',userdata=userdatalist,transactionlist=transactionlist,user=user,optionlist=optionlist,senderdata=senderdata)
 
 def dbConn():
 	return sqlite3.connect('data.sqlite')
@@ -74,6 +87,28 @@ def generateList(data):
 		for item in row:
 			item=str(item)
 			udata+=flask.Markup("<td>")+flask.Markup.escape(item)+flask.Markup("</td>")
+		udata+=flask.Markup("</tr>")
+	return udata	
+
+def generateTransactionList(data):
+	data=list(data)
+	data.reverse()
+	udata=''
+	for row in data:
+		udata+=flask.Markup("<tr>")
+		for item in row:
+			item=str(item)
+			udata+=flask.Markup("<td>")+flask.Markup.escape(item)+flask.Markup("</td>")
+		udata+=flask.Markup("</tr>")
+	return udata
+
+def generateUserList(data):
+	udata=''
+	for row in data:
+		udata+=flask.Markup("<tr>")
+		item=str(row[0])
+		udata+=flask.Markup("<td>")+flask.Markup.escape(item)+flask.Markup("</td>")
+		udata+=flask.Markup('<td><button name="user" type="submit" value="'+item+'">View</button></td>')
 		udata+=flask.Markup("</tr>")
 	return udata
 
--- trails.py
+++
@@ -1,9 +0,0 @@
-import sqlite3
-
-conn=sqlite3.connect('data.sqlite')
-data=conn.execute("SELECT Name from userdata")
-balance=conn.execute("SELECT Balance from userdata WHERE Name LIKE 'Devansh' ")
-b=balance.fetchone()
-#b=b[0]
-print(b)
-	
